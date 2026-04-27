2ef27c585e0a2c241b6880da10b5e2cc42839cec
AryaHalmare <aryaghalmare22@gmail.com>
2026-04-27 23:11:27 +0530

    Added Changes

--- src/test/java/bmt/AddTest.java
+++ src/test/java/bmt/AddTest.java
@@ -10,9 +10,11 @@ public class AddTest {
         int result1 = Add.add(10, 20);
         int result2 = Add.add(20, 20);
         int result3 = Add.add(30, 20);
+        int result4 = Add.add(50, 20);
 
         assertEquals(30, result1);
         assertEquals(40, result2);
         assertEquals(50, result3);
+        assertEquals(70 , result4);
     }
 }
